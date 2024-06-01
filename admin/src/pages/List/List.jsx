42e52547a5ce6050d09d43917c46f58016eff045
Quang Dong <quangdongne88@gmail.com>
2024-06-01 09:02:17 +0700

    feature: add admin page

---
+++ admin/src/pages/List/List.jsx
@@ -0,0 +1,63 @@
+import "./List.css";
+import { useState, useEffect } from "react";
+import axios from "axios";
+import { toast } from "react-toastify";
+
+const List = ({ url }) => {
+  /* items list */
+  const [list, setList] = useState([]);
+  const fetchList = async () => {
+    const response = await axios.get(`${url}/api/food/list`);
+
+    if (response.data.success) {
+      setList(response.data.data);
+    } else {
+      toast.error("Error");
+    }
+  };
+  useEffect(() => {
+    fetchList();
+  }, []);
+
+  //remove items from the list
+
+  const removeFood = async (FoodId) => {
+    const response = await axios.post(`${url}/api/food/remove`, { id: FoodId });
+    await fetchList();
+    if (response.data.success) {
+      toast.success(response.data.message);
+    } else {
+      toast.error("Error");
+    }
+  };
+
+  return (
+    <div className="list add flex-col">
+      <p>All food list</p>
+      <div className="list-table">
+        <div className="list-table-format title">
+          <b>Image</b>
+          <b>Name</b>
+          <b>Category</b>
+          <b>Price</b>
+          <b>Action</b>
+        </div>
+        {list.map((item, index) => {
+          return (
+            <div key={index} className="list-table-format">
+              <img src={`${url}/image/` + item.image} alt="" />
+              <p>{item.name}</p>
+              <p>{item.category}</p>
+              <p>{item.price}</p>
+              <p onClick={() => removeFood(item._id)} className="cursor">
+                X
+              </p>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default List;
